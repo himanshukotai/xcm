15b446419a73d3458d32d921c94c15fbd4822bb9
Himanshu Kotai <i358971@fhict.nl>
2023-04-02 22:17:00 +0200

    building

---
+++ build/precache-manifest.8604bcacb390c7bfde00ed0156b16554.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "471fe672f402f17bbd8e",
+    "url": "/static/css/main.c86dbcfa.chunk.css"
+  },
+  {
+    "revision": "471fe672f402f17bbd8e",
+    "url": "/static/js/main.471fe672.chunk.js"
+  },
+  {
+    "revision": "9f214d70663891210c4f",
+    "url": "/static/js/1.9f214d70.chunk.js"
+  },
+  {
+    "revision": "f86e40daaad090f00e8a",
+    "url": "/static/css/2.b2b92986.chunk.css"
+  },
+  {
+    "revision": "f86e40daaad090f00e8a",
+    "url": "/static/js/2.f86e40da.chunk.js"
+  },
+  {
+    "revision": "36cc2bf318d24e6efa20",
+    "url": "/static/js/runtime~main.36cc2bf3.js"
+  },
+  {
+    "revision": "9fce5c3ca7b51584607b225dd9d9e684",
+    "url": "/static/media/XCM_logo.9fce5c3c.jpg"
+  },
+  {
+    "revision": "34f8cfb88bbc2beaa8c74d6e7b42935e",
+    "url": "/static/media/socialmedia2.34f8cfb8.png"
+  },
+  {
+    "revision": "86d62a13d5a16ebb14ce2c225f13e870",
+    "url": "/static/media/figma.86d62a13.png"
+  },
+  {
+    "revision": "44a25fba4ab42ff4bc6b93cf97a3ba80",
+    "url": "/static/media/coding.44a25fba.png"
+  },
+  {
+    "revision": "89b67c4b8ab156f3791da2c453314e4d",
+    "url": "/static/media/webdev.89b67c4b.svg"
+  },
+  {
+    "revision": "e8fd7a6a7fbc95f2b2c40073b05ff268",
+    "url": "/static/media/copywriting.e8fd7a6a.png"
+  },
+  {
+    "revision": "6c3fdc614b195b1e2ffd4597fea0f096",
+    "url": "/static/media/mediamanagement.6c3fdc61.svg"
+  },
+  {
+    "revision": "54052e3eda609451377ce757b1d26711",
+    "url": "/static/media/analytics.54052e3e.svg"
+  },
+  {
+    "revision": "7f477633ddd12f84284654f2a2e89b8a",
+    "url": "/static/media/bootstrap-icons.7f477633.woff2"
+  },
+  {
+    "revision": "52196284de1fcb5b044f001a75482dba",
+    "url": "/static/media/bootstrap-icons.52196284.woff"
+  },
+  {
+    "revision": "4ca12182056e35434435553b28d0b323",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
